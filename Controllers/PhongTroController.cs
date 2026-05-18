bbb56208111fd6be5fc4db7109b7b937fba51539
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-05-18 15:13:37 +0700

    giao dien home

---
+++ Controllers/PhongTroController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyNhaTro.Controllers;
+
+public class PhongTroController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult ChiTiet()
+    {
+        return View();
+    }
+
+    public IActionResult BanDo()
+    {
+        return View();
+    }
+}
